75305f9f0aa6d9c50e6e764f45a0426a21957042
Lorenzo Paro <lorenzo.paro@student.h-farm.com>
2023-12-02 18:31:34 +0100

    Fixed the problems with year 2021 data
    Fixed the issue: https://github.com/nataliababic/swdevel-lab-hfarm/issues/5#issue-2022134985
    
    Now the data for the year 2021 are like this:
    - The value in the duration is 0 as we have no data
    - The value for the number of visitors is displayed correctly

--- backend/app/mymodules/csv_cleaning.py
+++ backend/app/mymodules/csv_cleaning.py
@@ -34,9 +34,8 @@ def is_holiday(date):
     """
     italy_holidays = holidays.Italy(years=[2019, 2020, 2021])
     try:
-        date_obj = pd.to_datetime(date)
-        is_weekend = date_obj.weekday() >= 5  # Saturday or Sunday
-        is_official_holiday = date_obj in italy_holidays
+        is_weekend = date.weekday() >= 5  # Saturday or Sunday
+        is_official_holiday = date in italy_holidays
         return is_weekend or is_official_holiday
     except ValueError:
         return False
@@ -44,19 +43,35 @@ def is_holiday(date):
 def preprocess_data(data):
     """Preprocess the data.
 
+    Shifts 'Duration' values to 'Visitors' column for the year 2021.
+    Sets 'Duration' to "0" for the year 2021.
+
     Parameters:
     data (DataFrame): The input DataFrame.
 
     Returns:
     DataFrame: Preprocessed DataFrame.
     """
-    data['Holiday'] = data['Date'].apply(is_holiday)
-    data['Date'] = pd.to_datetime(data['Date'])
-    data["Visitors"] = data["Visitors"].fillna(0).astype(int)
-    data = data.sort_values(by="Date", ascending=True)
-    data['Date'] = data['Date'].dt.strftime('%d/%m/%Y')
-    data.loc[data['Visitors'] < 0, 'Visitors'] = 0
-    return data
+    data_copy = data.copy()  # Create a copy to avoid modifying the original DataFrame
+    data_copy['Date'] = pd.to_datetime(data_copy['Date'], dayfirst=True)
+    data_copy['Holiday'] = data_copy['Date'].apply(is_holiday)
+
+    # Shift 'Duration' to 'Visitors' column only for 2021
+    mask_2021 = data_copy['Date'].dt.year == 2021
+    data_copy.loc[mask_2021, 'Visitors'] = pd.to_numeric(data_copy.loc[mask_2021, 'Duration'], errors='coerce')
+
+    # Set 'Duration' to "0" only for 2021
+    data_copy.loc[mask_2021, 'Duration'] = "0"
+
+    # Convert 'Duration' column to integer
+    data_copy["Visitors"] = data_copy["Visitors"].fillna(0).astype(int)
+
+    data_copy = data_copy.sort_values(by="Date", ascending=True)
+    data_copy['Date'] = data_copy['Date'].dt.strftime('%d/%m/%Y')
+    data_copy.loc[data_copy['Visitors'] < 0, 'Visitors'] = 0
+
+    return data_copy
+
 
 def convert_to_minutes(duration_str):
     """Convert duration strings to minutes.
